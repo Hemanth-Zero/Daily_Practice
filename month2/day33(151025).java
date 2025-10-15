6b01d01a25ed22f3b3892bbe000acf1444dea788
Hemanth-Zero <hemanthsagar0111@gmail.com>
2025-10-15 17:10:13 +0000

    wasnt ez

---
+++ month2/day33(151025).java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int n = nums.size();
+        if (n < 2) return 0;
+
+        int ans = 0;
+        int pre = 0;
+        int cur = 0;
+
+        for (int i = 0; i < n; i++) {
+            cur++;
+            if (i == n - 1 || nums.get(i) >= nums.get(i + 1)) {
+                ans = Math.max(ans, cur / 2);
+                ans = Math.max(ans, Math.min(pre, cur));
+                pre = cur;
+                cur = 0;
+            }
+        }
+        return ans;
+    }
+}
