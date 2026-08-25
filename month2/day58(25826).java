0bf31428f606b54eb97a2d81d26da7b41e3c5f2f
Hemanth-Zero <hemanthsagar0111@gmail.com>
2026-08-25 17:40:43 +0000

    easy dont look

---
+++ month2/day58(25826).java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        Arrays.sort(nums);
+        int mul = 1;
+        for(int i=0;i<nums.length;i++){
+            if(k*mul == nums[i]){
+                i=0;
+                mul++;
+            }
+        }
+        return k*mul;
+    }
+}
