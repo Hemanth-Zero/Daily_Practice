873d7cf2f766f581929a146e4ea96cef7081cacc
Hemanth-Zero <hemanthsagar0111@gmail.com>
2026-08-27 19:20:14 +0000

    no

--- month2/day60(28826).java
+++ month2/day60(28826).java
@@ -1,44 +1,53 @@
 class Solution {
     public String lexGreaterPermutation(String s, String target) {
         int[] count = new int[26];
+
         for (char c : s.toCharArray()) {
             count[c - 'a']++;
         }
 
         int n = s.length();
-        StringBuilder ans = new StringBuilder();
+        int matched = 0;
 
-        for (int i = 0; i < n; i++) {
-            int x = target.charAt(i) - 'a';
+        while (matched < n) {
+            int c = target.charAt(matched) - 'a';
 
-            if (count[x] > 0) {
-                count[x]--;
-                ans.append(target.charAt(i));
-            } else {
+            if (count[c] == 0) {
                 break;
             }
+
+            count[c]--;
+            matched++;
         }
 
-        for (int i = ans.length() - 1; i >= 0; i--) {
-            count[target.charAt(i) - 'a']++;
+        for (int i = matched; i >= 0; i--) {
+            if (i < matched) {
+                count[target.charAt(i) - 'a']++;
+            }
+
+            if (i == n) {
+                continue;
+            }
 
             int x = target.charAt(i) - 'a';
 
             for (int c = x + 1; c < 26; c++) {
                 if (count[c] > 0) {
-                    StringBuilder res = new StringBuilder();
+                    StringBuilder ans = new StringBuilder();
 
-                    res.append(ans.substring(0, i));
-                    res.append((char) ('a' + c));
+                    ans.append(target, 0, i);
+                    ans.append((char) ('a' + c));
+
                     count[c]--;
 
                     for (int j = 0; j < 26; j++) {
-                        while (count[j]-- > 0) {
-                            res.append((char) ('a' + j));
+                        while (count[j] > 0) {
+                            ans.append((char) ('a' + j));
+                            count[j]--;
                         }
                     }
 
-                    return res.toString();
+                    return ans.toString();
                 }
             }
         }
